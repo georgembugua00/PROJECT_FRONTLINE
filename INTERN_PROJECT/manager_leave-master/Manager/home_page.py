189c532bb74ab9ac9c720369bc1327ad02dde858
George Mbugua <151632200+georgembugua00@users.noreply.github.com>
2025-07-30 14:29:39 +0300

    Update home_page.py

--- INTERN_PROJECT/manager_leave-master/Manager/home_page.py
+++ INTERN_PROJECT/manager_leave-master/Manager/home_page.py
@@ -8,9 +8,9 @@ import sqlite3
 
 conn = sqlite3.connect('leave_management.db')
 
-employee_data = pd.read_csv("data/employee_table.csv")
-leave_entry_data = pd.read_csv("data/leave_entries.csv")
-leave_entitlements_data = pd.read_csv("data/leave_entitlements_data.csv")
+employee_data = pd.read_csv("./data/employee_table.csv")
+leave_entry_data = pd.read_csv("./data/leave_entries.csv")
+leave_entitlements_data = pd.read_csv("./data/leave_entitlements_data.csv")
 
 employee_data.to_sql(name="employee_table",con=conn,if_exists='replace',index=False)
 leave_entry_data.to_sql(name="leave_entry",con=conn,if_exists='replace',index=False)
